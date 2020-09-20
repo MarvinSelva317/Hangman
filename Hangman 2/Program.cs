0b4dc5e68c303edd13b50a5eb7b7a6ba12320a80
MarvinSelva317 <marvinselva317@gmail.com>
2020-09-19 20:50:38 -0400

    Add project files.

---
+++ Hangman 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using Hangman_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProgramUI game = new ProgramUI();
+            game.Run();
+        }
+    }
+}
